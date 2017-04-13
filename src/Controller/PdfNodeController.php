556bee5b76e7e4cbdb1d0ce65f07baef6834d0ae
Wesley Sandra <wesley.sandra@o2agency.be>
2017-04-13 14:56:45 +0200

    Initial commit.

---
+++ src/Controller/PdfNodeController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Drupal\entity_pdf\Controller;
+
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\node\Controller\NodeViewController;
+use Mpdf\Output\Destination;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Defines a controller to render a single node.
+ */
+class PdfNodeController extends NodeViewController {
+
+  public function view(EntityInterface $node, $view_mode, $langcode = NULL) {
+    $build = [
+      '#theme' => 'htmlpdf',
+      '#title' => parent::title($node),
+      '#content' => parent::view($node, $view_mode, $langcode),
+    ];
+
+    $mpdf = new \Mpdf\Mpdf();
+    $mpdf->SetBasePath(\Drupal::request()->getSchemeAndHttpHost());
+    $mpdf->SetTitle($this->title($node));
+    $mpdf->WriteHTML(render($build));
+    $content = $mpdf->Output('', Destination::INLINE);
+
+    $headers = [
+      'Content-Type: application/pdf',
+      'Content-disposition: inline; filename="' . $this->title($node) . '"',
+    ];
+
+    return new Response($content, 200, $headers);
+  }
+
+  /**
+   * @inheritdoc
+   */
+  public function title(EntityInterface $node) {
+    return parent::title($node);
+  }
+}
