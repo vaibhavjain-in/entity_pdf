27b952ddc1cd591e0c0b84bf490d5320f73e426a
weseze <weseze@gmail.com>
2018-10-12 17:34:15 +0200

    Output UTF A4 with no margins by default. Allow for a BC mode.

--- src/Controller/PdfNodeController.php
+++ src/Controller/PdfNodeController.php
@@ -35,7 +35,21 @@ class PdfNodeController extends NodeViewController {
 
     $config = [
       'tempDir' => DRUPAL_ROOT . '/sites/default/files/entity_pdf',
+      'mode' => 'utf-8',
+      'format' => 'A4',
+      'margin_left' => 0,
+      'margin_right' => 0,
+      'margin_top' => 0,
+      'margin_bottom' => 0,
+      'margin_header' => 0,
+      'margin_footer' => 0,
     ];
+    if (\Drupal::config('entity_pdf.settings')->get('bc') == 8001) {
+      $config = [
+        'tempDir' => DRUPAL_ROOT . '/sites/default/files/entity_pdf',
+      ];
+    }
+
     $mpdf = new Mpdf($config);
     $mpdf->SetBasePath(\Drupal::request()->getSchemeAndHttpHost());
     $mpdf->SetTitle($filename);
