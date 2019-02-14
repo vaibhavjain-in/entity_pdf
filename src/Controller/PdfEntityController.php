2dd66120c26a23f20f3332d72d7103ed6c2629d4
weseze <weseze@417521.no-reply.drupal.org>
2019-02-14 16:55:29 +0100

    Issue #3032240 by weseze: Support for every entity type including custom entities

--- src/Controller/PdfEntityController.php
+++ src/Controller/PdfEntityController.php
@@ -2,29 +2,29 @@
 
 namespace Drupal\entity_pdf\Controller;
 
+use Drupal\Core\Entity\Controller\EntityViewController;
 use Mpdf\Config\ConfigVariables;
 use Mpdf\Config\FontVariables;
 use Mpdf\Mpdf;
 use Drupal\Core\Entity\EntityInterface;
-use Drupal\node\Controller\NodeViewController;
 use Mpdf\Output\Destination;
 use Symfony\Component\HttpFoundation\Response;
 
 /**
- * Defines a controller to render a single node.
+ * Defines a controller to render a single entity.
  */
-class PdfNodeController extends NodeViewController {
+class PdfEntityController extends EntityViewController {
 
   /**
    * Public function view.
    */
-  public function view(EntityInterface $node, $view_mode = 'full', $langcode = NULL) {
+  public function view(EntityInterface $entity, $view_mode = 'full', $langcode = NULL) {
     global $base_url;
 
     $build = [
       '#theme' => 'htmlpdf',
-      '#title' => parent::title($node),
-      '#content' => parent::view($node, $view_mode, $langcode),
+      '#title' => $entity->label(),
+      '#content' => parent::view($entity, $view_mode, $langcode),
       '#base_url' => $base_url,
     ];
 
@@ -36,7 +36,7 @@ class PdfNodeController extends NodeViewController {
     // Get the filename from config and replace tokens.
     $configFactory = \Drupal::service('config.factory');
     $config = $configFactory->get('entity_pdf.settings');
-    $filename = \Drupal::token()->replace($config->get('filename'), [ 'node' => $node ], ['langcode' => $langcode]);
+    $filename = \Drupal::token()->replace($config->get('filename'), [ $entity->getEntityTypeId() => $entity ], ['langcode' => $langcode]);
 
     // Get mpdf's default config and allow other modules to alter it.
     $mpdf_config = [];
@@ -52,11 +52,12 @@ class PdfNodeController extends NodeViewController {
     $mpdf->SetBasePath(\Drupal::request()->getSchemeAndHttpHost());
     $mpdf->SetTitle($filename);
     $mpdf->WriteHTML($output);
-    $content = $mpdf->Output($filename, Destination::INLINE);
+    $content = $mpdf->Output($filename, Destination::STRING_RETURN);
     $headers = [
       'Content-Type' => 'application/pdf',
       'Content-disposition' => 'attachment; filename="' . $filename . '"',
     ];
+
     return new Response($content, 200, $headers);
   }
 
@@ -65,8 +66,8 @@ class PdfNodeController extends NodeViewController {
    *
    * @inheritdoc
    */
-  public function title(EntityInterface $node) {
-    return parent::title($node);
+  public function title(EntityInterface $entity) {
+    return $entity->label();
   }
 
 }
